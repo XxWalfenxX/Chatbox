c5d6cb441d31d6a89f9a9519857fdfdf96607c70
Firagon <93060162+firagonb3@users.noreply.github.com>
2023-01-25 16:59:58 +0100

    Recuperar datos funcional

--- app/src/main/java/com/wolfiex/chatbox/MainActivity2.kt
+++ app/src/main/java/com/wolfiex/chatbox/MainActivity2.kt
@@ -13,18 +13,26 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.wolfiex.chatbox.databinding.ActivityMain2Binding
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 class MainActivity2 : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMain2Binding
     private lateinit var auth: FirebaseAuth
-
+    lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val user = Firebase.auth.currentUser
+
+        val db = Firebase.firestore
+        storage = Firebase.storage
 
 
         super.onCreate(savedInstanceState)
@@ -32,11 +40,13 @@ class MainActivity2 : AppCompatActivity() {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
+
         setSupportActionBar(binding.appBarMain.toolbarBarraNavegacion)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         // Cargar items barra lateral
@@ -48,25 +58,40 @@ class MainActivity2 : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        val nav_view: NavigationView = findViewById(R.id.nav_view)
-        val userText: TextView = nav_view.getHeaderView(0).findViewById(R.id.textoNombreUsuario)
-        val emailText: TextView = nav_view.getHeaderView(0).findViewById(R.id.textoMailUser)
-        val estatText: TextView = nav_view.getHeaderView(0).findViewById(R.id.textoEstadoUsuario)
-
-        if (user != null) {
-            userText.setText(user.displayName.toString())
-            emailText.setText(user.email.toString())
-            estatText.setText("test")
-        }
 
     }
 
     override fun onStart() {
         super.onStart()
+        val user = Firebase.auth.currentUser
+        if (user != null) {
+            val db = Firebase.firestore
+            val nav_view: NavigationView = findViewById(R.id.nav_view)
+            val userText: TextView = nav_view.getHeaderView(0).findViewById(R.id.textoNombreUsuario)
+            val emailText: TextView = nav_view.getHeaderView(0).findViewById(R.id.textoMailUser)
+            val estatText: TextView =
+                nav_view.getHeaderView(0).findViewById(R.id.textoEstadoUsuario)
+            var hastest: Map<String, Any>
+            db.collection("users").document(user.email.toString())
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                        hastest = document.data as Map<String, Any>
+                        estatText.setText(hastest.get("estado").toString())
+                        userText.setText(hastest.get("name").toString())
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
 
+            emailText.setText(user.email.toString())
+
+        }
     }
 
-    private fun getCurrentUser(){
+    private fun getCurrentUser() {
         val user = Firebase.auth.currentUser
         user?.let {
             // Name, email address, and profile photo Url
@@ -91,5 +116,15 @@ class MainActivity2 : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    companion object {
+
+        private val TAG = "DocSnippets"
+
+        private val EXECUTOR = ThreadPoolExecutor(
+            2, 4,
+            60, TimeUnit.SECONDS, LinkedBlockingQueue()
+        )
     }
 }
